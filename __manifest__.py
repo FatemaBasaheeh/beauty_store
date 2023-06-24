a50eb240170585d31629a88f3ab41b15c82264d2
FatemaBasaheeh <137589173+FatemaBasaheeh@users.noreply.github.com>
2023-06-24 15:46:55 +0300

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+  'name': 'beauty pro',
+  'version': '1.0',
+  'category': 'Accounting/Accounting',
+  'author': 'Fatema Basaheeh ',
+  'website': "",
+  'company': 'Fatema Basaheeh',
+  'maintainer': 'Fatema Basaheeh',
+  'summary': 'Record beauty Details',
+  "description": "",
+  'depends': ['base'],
+   'data': [
+      'security/beauty_pro.csv',
+      'views/beauty_pro_views.xml',
+  ],
+  'installable':True,
+  'application':True,
+  'license':'LGPL-3',
+ 
+}
